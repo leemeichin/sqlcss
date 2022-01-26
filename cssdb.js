0d549257ca325c43139068c5413beff277dc757a
Lee Meichin <lee@hopin.to>
2022-01-26 14:02:47 +0000

    render query and add licenses

--- cssdb.js
+++ cssdb.js
@@ -1,4 +1,5 @@
 import initSqlJs from './sql-asm.js'
+import font from './font.js'
 
 class SqlDB {
   static _db;
@@ -41,9 +42,24 @@ class SqlDB {
     const db = await SqlDB.db()
     const result = db.exec(query);
 
-    console.log(query)
-    console.log(result[0].values.join(', '))
+    // console.log(font)
+    // console.log(query)
+    // console.log(result[0].values.join(', '))
 
+    const queryWidth = font.getAdvanceWidth(query, 48)
+    console.log(geom.width, queryWidth)
+    const queryPath = font.getPath(query, (geom.width / 2) - (queryWidth / 2), geom.height / 2, 48)
+    queryPath.fill = '#fdfdfd'
+    queryPath.draw(ctx)
+
+    const results = result[0].values.join('; ')
+    const resultsWidth = font.getAdvanceWidth(results, 48)
+    const resultsPath = font.getPath(results, (geom.width / 2) - (resultsWidth / 2), geom.height / 2 + 64, 48)
+    
+    resultsPath.fill = '#f8f8f8'
+    resultsPath.stroke = '#050505'
+    resultsPath.strokeWidth = 1
+    resultsPath.draw(ctx)
   }
 }
 
